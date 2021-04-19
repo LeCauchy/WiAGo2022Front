53992d5ecffb3c3794ff9a004d84b6b3e5063e7e
LeCauchy <60450466+LeCauchy@users.noreply.github.com>
2021-04-19 14:27:19 +0200

    ajout du module de la carte

--- src/admin/components/pages/MapPage.js
+++ src/admin/components/pages/MapPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
-import testData from '../data/testdata.json'
+import teslaData from '../data/testdata.json'
 
 class MapPage extends Component {
 
@@ -10,27 +10,69 @@ class MapPage extends Component {
         lng: 11.5113984
     }
 
-    filteredStations = testData.filter(tsla => tsla.address.country === "Italy")
-       
+    redIcon = new L.Icon({
+        iconUrl: ("https://raw.githubusercontent.com/codegeous/react-component-depot/master/src/resources/images/marker.png"),
+        iconSize: [40, 40],
+        iconAnchor: [17, 46],
+        popupAnchor: [0, -46],
+    });
+    blueIcon = new L.Icon({
+        iconUrl: ("https://toppng.com/uploads/preview/ushpin-png-pic-blue-push-pin-11562974655c9ynfiavhn.png"),
+        iconSize: [40, 40],
+        iconAnchor: [17, 46],
+        popupAnchor: [0, -46],
+    });
+
+    openStations = teslaData.filter(tesla => tesla.status === "OPEN")
+    closeStations = teslaData.filter(tesla => tesla.status === "CLOSED")
+    //filteredStations = testData.filter(tsla => tsla.address.country === "Italy")
+
     render() {
         return (
-            <MapContainer center={[this.test.lat, this.test.lng]} zoom={5} scrollWheelZoom={false} style={{height: '100vh', width:'100%'}}>
+            <MapContainer center={[3.8623896, 11.4992693]} zoom={17} scrollWheelZoom={true} style={{height: '100vh', width:'100%'}}>
                 <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                maxZoom = "19"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    maxZoom = "20"
                 />
 
-                {testData.map(tesla => (
+                {this.openStations.map(tesla => (
                     <Marker
-                    key = {tesla.id}
-                    position = { [tesla.gps.latitude, tesla.gps.longitude]}>
+                        key={tesla.id}
+                        position={[tesla.gps.latitude, tesla.gps.longitude]}>
+
+                        <Popup position={[tesla.gps.latitude, tesla.gps.longitude]}>
+                            <div>
+                                <h2>{"Name : " + tesla.name}</h2>
+                                <p>{"Status : " + tesla.status}</p>
+                                <p>{"Location : " + tesla.locationId}</p>
+                            </div>
+                        </Popup>
 
                     </Marker>
-                ))} 
+
+                ))}
+
+                {this.closeStations.map(tesla => (
+                    <Marker
+                        key={tesla.id}
+                        position={[tesla.gps.latitude, tesla.gps.longitude]}
+                        icon={this.redIcon}>
+
+                        <Popup position={[tesla.gps.latitude, tesla.gps.longitude]}>
+                            <div>
+                                <h2>{"Name : " + tesla.name}</h2>
+                                <p>{"Status : " + tesla.status}</p>
+                                <p>{"Location : " + tesla.locationId}</p>
+                            </div>
+                        </Popup>
+
+                    </Marker>
+
+                ))}
             </MapContainer>
         )
-        
+
     }
 }
 
